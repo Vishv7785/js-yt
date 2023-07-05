54e4e1aa6641723e9d5dcc47020e675cc25cb678
Vishv Gajjar <104492801+Vishv7785@users.noreply.github.com>
2023-07-05 16:32:06 +0000

    dataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 7785
+let accountEmail ="abc@123.com"
+var accountPasswords ="123"
+accountCity = "Deesa"
+
+// accountId = 2  // not allowed
+
+accountEmail ="abc@123.com"
+accountPasswords ="123"
+accountCity = "Deesa"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPasswords, accountCity]);
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,26 @@
+"Use Scrict"; // treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not  browser
+
+ //code readeility should be high
+
+
+
+let name = "vishv"
+let age = 18
+let isLoggesIn = false
+let state;
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null =>standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof "vishv"); // undefined
+
+// object
